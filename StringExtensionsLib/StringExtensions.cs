72f386f393b82358e00e0c2b9069883aa82804cd
Kevin Rodrigues <a.kevinrodrigues@gmail.com>
2026-04-08 19:12:06 -0400

    Adicionar arquivos de projeto.

---
+++ StringExtensionsLib/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringExtensionsLib
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpper(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return false;
+
+            return char.IsUpper(input[0]);
+        }
+    }
+}
